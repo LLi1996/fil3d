8a6fbba03645a7a9351812b88fa3ed1ff97f81d4
Larry Li <larry9607@gmail.com>
2021-04-16 17:10:36 -0400

    more tests

--- tests/structs/test_mask_obj_node.py
+++ tests/structs/test_mask_obj_node.py
@@ -19,7 +19,8 @@ from fil3d.structs import util
     (tests.abs_path_from_project_root('data/testing/unpickle/node_b.pickle'),
      tests.abs_path_from_project_root('data/testing/unpickle/node_b_py2.pickle')),
     (tests.abs_path_from_project_root('data/testing/unpickle/node_c.pickle'),
-     tests.abs_path_from_project_root('data/testing/unpickle/node_c_py2.pickle'))])
+     tests.abs_path_from_project_root('data/testing/unpickle/node_c_py2.pickle'))
+])
 def test_unpickling(v001_pickle_path, pre_v001_py2_pickle_path):
     """Testing unpicking from pickles created in various points in time (from the same node)"""
     node_v001 = pickle.load(open(v001_pickle_path, 'rb'))
@@ -34,6 +35,22 @@ def test_unpickling(v001_pickle_path, pre_v001_py2_pickle_path):
     assert node_v001.visited == node_pre_v001.visited
     assert node_v001.mask_size == node_pre_v001.mask_size
     assert node_v001.masked_area_size == node_pre_v001.masked_area_size
+
+
+@pytest.mark.parametrize('pre_v001_py2_pickle_path', [
+    tests.abs_path_from_project_root('data/testing/unpickle/node_a_py2.pickle'),
+    tests.abs_path_from_project_root('data/testing/unpickle/node_b_py2.pickle'),
+    tests.abs_path_from_project_root('data/testing/unpickle/node_c_py2.pickle')
+])
+def test_pre_v001_function_name_backwards_compatibility(pre_v001_py2_pickle_path):
+    """Testing the function rename aliasing still works for pre v001 nodes"""
+    node_pre_v001 = util.pre_v001_pickle_load(open(pre_v001_py2_pickle_path, 'rb'))
+
+    assert node_pre_v001.checkMaskedAreaSize() == node_pre_v001.check_masked_area_size()
+    assert node_pre_v001.checkAreaSize() == node_pre_v001.check_area_size()
+    assert node_pre_v001.getDimentions() == node_pre_v001.get_dimensions()
+    assert node_pre_v001.getAR() == node_pre_v001.get_ar()
+    # todo add more to this backwards compatibility
 
 
 @pytest.mark.parametrize('mask, corners, v_index, expected_mask_size, expected_mask_area_size', [
