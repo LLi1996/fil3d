96e9fb18e051b142a0fb95b3969b0be9585f7d9b
Larry Li <larry9607@gmail.com>
2021-06-24 12:32:27 -0400

    Bugfix/fix setup py packages (#9)
    * changed to inlcude as opposed to where

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+"""
+
+"""
+
 import setuptools
 
 
@@ -11,19 +15,16 @@ def get_version(path):
             raise RuntimeError("Unable to find version string.")
 
 
-__version__ = get_version('fil3d/__init__.py')
-
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(
     name='Fil3D',
-    version=__version__,
+    version=get_version('fil3d/__init__.py'),
     description='A python package for building 3d filaments in (x,y,v)',
     long_description=long_description,
     url='https://lli1996.github.io/fil3d/',
-    package_dir={'': 'fil3d'},
-    packages=setuptools.find_packages(where='fil3d'),
+    packages=setuptools.find_packages(include=['fil3d', 'fil3d.*']),
     python_requires='>=3.6',
     install_requires=[
         'astropy',
